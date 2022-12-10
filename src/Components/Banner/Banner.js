9f447707c05e03ce5c47856b7779baffb9884536
Arif351 <ariful8836@gmail.com>
2022-12-10 14:25:34 +0600

    download button added

--- src/Components/Banner/Banner.js
+++ src/Components/Banner/Banner.js
@@ -1,11 +1,19 @@
 import React from 'react';
 import banner from '../../Assets/banner.png';
+import './Banner.css'
+import { FaDownload } from 'react-icons/fa';
+import resume from '../../Assets/Resume/Ariful Islam Resume.pdf'
+
 
 const Banner = () => {
     return (
         <div>
-            <img className='w-full' src={banner} alt="" />
-        </div>
+            <div>
+                <img className='w-full' src={banner} alt="" />
+                <a href={resume} download={resume}><button className="btn btn-outline btn-info">
+                    Download Resume <FaDownload className='ml-3'></FaDownload> </button></a>
+            </div>
+        </div >
     );
 };
 
