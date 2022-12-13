c6c4e64c2814194733bab0c1c3a64e445ddc0609
Arif351 <ariful8836@gmail.com>
2022-12-14 00:19:06 +0600

    tyy

--- src/Components/Banner/Banner.js
+++ src/Components/Banner/Banner.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import banner from '../../Assets/r2.jpg';
 import me from '../../Assets/me.png';
-// import video from '../../Assets/videos/video.mp4'
+import video from '../../Assets/videos/video.mp4'
 import './Banner.css'
 import { FaDownload } from 'react-icons/fa';
 import resume from '../../Assets/Resume/Ariful_Islam_Resume.pdf'
@@ -14,7 +14,10 @@ const Banner = () => {
             <div className=''>
                 {/* <iframe src="https://www.veed.io/embed/95888357-fc7c-4caf-9e3c-693ffbbec092" className='w-full' frameborder="0" title="video.mp4" webkitallowfullscreen mozallowfullscreen allowfullscreen autoPlay loop muted></iframe> */}
                 {/* <video className='brightness-50' autoPlay loop muted>
-                    <source src="https://streamable.com/wmcyn4" type='video/mp4' />
+                    <source src="https://www.pexels.com/video/starry-night-sky-timelapse-5818973/" type='video/mp4' />
+                </video> */}
+                {/* <video className='brightness-50' autoPlay loop muted>
+                    <source src={video} type='video/mp4' />
                 </video> */}
                 <img className='w-full bannerImg brightness-50' src={banner} alt="" />
                 <div className='text-center'>
@@ -33,7 +36,7 @@ const Banner = () => {
                             }}
                         /> */}
                     </h2>
-                    <h4 className="typeWriter2 text-2xl text-white font-semibold text-center"> A Full Stack Web Developer
+                    <h4 className="typeWriter2 text-2xl text-white font-semibold text-center web"> A Full Stack Web Developer
                         {/* <Typewriter
                             options={{
                                 strings: [''],
